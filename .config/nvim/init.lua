20f99ba9a08e8dfd9b51809e1ff0f2806345432a
joowy <46057052+joowy@users.noreply.github.com>
2026-05-07 19:43:10 -0400

    update nvim hardtime and terminal

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.opt.shell = "/bin/zsh"
+vim.opt.autochdir = true
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,11 +2,31 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_create_user_command("TerminalSplit", function()
-  vim.cmd.new()
-  vim.cmd.term()
-  vim.cmd.vnew()
-  vim.cmd.term()
-end, { desc = "Create two terminals at the bottom" })
-
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>")
+
+vim.keymap.set({ "n", "t" }, "<leader>t1", function()
+  Snacks.terminal.toggle(nil, { count = 1 })
+end, { desc = "Toggle terminal 1" })
+
+vim.keymap.set({ "n", "t" }, "<leader>t2", function()
+  Snacks.terminal.toggle(nil, { count = 2 })
+end, { desc = "Toggle terminal 2" })
+
+vim.keymap.set({ "n", "t" }, "<C-/>", function()
+  local t1 = Snacks.terminal.get(nil, { count = 1, create = false })
+  local t2 = Snacks.terminal.get(nil, { count = 2, create = false })
+  local any_visible = (t1 and t1:valid()) or (t2 and t2:valid())
+  if any_visible then
+    if t1 and t1:valid() then
+      t1:hide()
+    end
+
+    if t2 and t2:valid() then
+      t2:hide()
+    end
+  else
+    Snacks.terminal.toggle(nil, { count = 1 })
+
+    Snacks.terminal.toggle(nil, { count = 2 })
+  end
+end, { desc = "Toggle both terminals" })
