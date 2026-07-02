df7498a700f21f8bdb32ff3866c23ff9a4d8e0cf
joowy <46057052+joowy@users.noreply.github.com>
2026-07-02 12:37:32 -0400

    update nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,20 @@
 
 vim.keymap.set("t", "<esc><esc>", "<C-\\><C-n>")
 
+-- Copy relative path to system clipboard with notification
+vim.keymap.set("n", "<leader>cF", function()
+  local path = vim.fn.expand("%")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied relative path: " .. path, vim.log.levels.INFO)
+end, { silent = true, desc = "Copy relative path to clipboard" })
+
+-- Copy absolute path to system clipboard with notification
+vim.keymap.set("n", "<leader>cf", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied absolute path: " .. path, vim.log.levels.INFO)
+end, { silent = true, desc = "Copy absolute path to clipboard" })
+
 vim.keymap.set("i", "<M-h>", "<Left>", { noremap = true, silent = true })
 vim.keymap.set("i", "<M-l>", "<Right>", { noremap = true, silent = true })
 
